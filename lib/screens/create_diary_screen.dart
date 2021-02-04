308e08715ea9f10262f8ee1fdb6ad1bbd54af702
NaHyun Kwon <35582991+NinanoKwon@users.noreply.github.com>
2021-02-04 22:44:58 +0900

    Update create_diary_screen.dart

--- lib/screens/create_diary_screen.dart
+++ lib/screens/create_diary_screen.dart
@@ -1,13 +1,35 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:together/Constants.dart';
 
-class AddDiaryScreen extends StatelessWidget {
+class CreateDiaryScreen extends StatefulWidget {
+  @override
+  _CreateDiaryScreenState createState() => _CreateDiaryScreenState();
+}
+
+class _CreateDiaryScreenState extends State<CreateDiaryScreen> {
+  File _image;
+  TextEditingController _diaryController = TextEditingController();
+
+  @override
+  void dispose() {
+    _diaryController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: kPrimaryColor,
-        title: Text('추억 작성하기'),
+        title: Text(
+          '추억 작성하기',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
         actions: [
           FlatButton(
             textColor: Colors.black,
@@ -38,47 +60,35 @@ class AddDiaryScreen extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(
-              height: 48.0,
-            ),
-            const Divider(
-              color: Colors.grey,
-              height: 10,
-              thickness: 2,
-              indent: 10,
-              endIndent: 10,
-            ),
-            //image picker
-            const Divider(
-              color: Colors.grey,
-              height: 10,
-              thickness: 2,
-              indent: 10,
-              endIndent: 10,
-            ),
+            _image == null ? Text('no image') : Image.file(_image),
             TextField(
               onChanged: (value) {},
               decoration: InputDecoration(
                 hintText: '내용 작성',
                 contentPadding: EdgeInsets.symmetric(
-                  vertical: 10.0,
+                  vertical: 20.0,
                   horizontal: 20.0,
                 ),
               ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            const Divider(
-              color: Colors.grey,
-              height: 10,
-              thickness: 2,
-              indent: 10,
-              endIndent: 10,
+              controller: _diaryController,
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: kPLightColor,
+        foregroundColor: Colors.black,
+        onPressed: _getImage,
+        child: Icon(Icons.add_a_photo),
+      ),
     );
+  }
+
+  Future _getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = image;
+    });
   }
 }
